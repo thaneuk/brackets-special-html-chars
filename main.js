c33e8d4aa54d20ca15c7d3e3d75bb854cc0cfcc4
Gregory Jackson <gregory@fates.co.uk>
2013-11-10 15:39:22 -0500

    Update main.js
    added alt-c shortcut

--- main.js
+++ main.js
@@ -39,10 +39,11 @@ define(function (require, exports, module) {
         SpecialChars = require('text!specialHTMLChars.json'),
         specialChars = JSON.parse(SpecialChars),
         COMMAND_ID = 'specialhtmlchar.insert',
+				FULLCOMMAND_ID = 'full.specialhtmlchar.insert',
         MENU_NAME = 'Special HTML Character',
         iChars,
         iCharsLen = specialChars.quickLinks.length,
-        menu,
+        menu, editMenu, keyCommands,
         $specialCharsDialog = $('<div>', { 'class': 'specialHTMLChar' }),
         $fullSpecialCharsDialog = $('<div>', { 'class': 'specialHTMLChar full' }),
         dialogDimensions;
@@ -143,7 +144,17 @@ define(function (require, exports, module) {
     CommandManager.register(MENU_NAME, COMMAND_ID, showDialog);
 
     menu = Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU);
-    menu.addMenuDivider();
-    menu.addMenuItem(COMMAND_ID);
+	  if (menu) {
+			menu.addMenuDivider();
+      menu.addMenuItem(COMMAND_ID);
+		}
+
+	  CommandManager.register(MENU_NAME, FULLCOMMAND_ID, showFullListDialog);
+	
+	  editMenu = Menus.getMenu(Menus.AppMenuBar.EDIT_MENU);
+    if (editMenu) {
+      editMenu.addMenuDivider();
+      editMenu.addMenuItem(FULLCOMMAND_ID, "Alt-C");
+    }
 
 });
