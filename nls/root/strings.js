ad142f7c5d9f0d8f49f3daf93fc7f4ccbbe27967
Denis R <student@alice.it>
2014-09-12 22:00:17 +0200

    Create strings.js

---
+++ nls/root/strings.js
@@ -0,0 +1,12 @@
+// English - root strings
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define */
+
+define({
+    "SPECIAL_HTML_CHARACTER"                      : "Special HTML Character",
+    "MORE"                                        : "more...",
+    "CLICK_THE_CHARACTER_YOU_WISH_TO_INSERT"      : "Click the character you wish to insert",
+    "CANCEL"                                      : "Cancel"
+   
+});
