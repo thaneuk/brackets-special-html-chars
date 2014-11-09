0dc0f42a10509354850e43c571a6070b64e2fbfd
Gregory Jackson <gregory@fates.co.uk>
2014-11-09 17:00:22 -0500

    Danish and Afrikaans strings

---
+++ nls/af/strings.js
@@ -0,0 +1,11 @@
+// Afrikaans - strings
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define */
+
+define({
+    SPECIAL_HTML_CHARACTER                      : 'Spesiale HTML karakter',
+    MORE                                        : 'Wys meer...',
+    CLICK_THE_CHARACTER_YOU_WISH_TO_INSERT      : 'Klik op die karakter wat jy wil in te voeg',
+    CANCEL                                      : 'Staak'
+});
---
+++ nls/nl/strings.js
@@ -0,0 +1,11 @@
+// Dutch - strings
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define */
+
+define({
+    SPECIAL_HTML_CHARACTER                      : 'Speciaal HTML teken',
+    MORE                                        : 'Meer...',
+    CLICK_THE_CHARACTER_YOU_WISH_TO_INSERT      : 'Klik op het teken dat je wil invoegen',
+    CANCEL                                      : 'Annuleren'
+});
