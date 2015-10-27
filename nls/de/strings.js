c2d3d16c84baf22902fac729db1ec43b856774cf
Daniel Kratz <git@danielkratz.com>
2015-10-27 18:45:55 +0100

    Initial German translation

---
+++ nls/de/strings.js
@@ -0,0 +1,11 @@
+// German - strings
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define */
+
+define({
+    SPECIAL_HTML_CHARACTER                      : 'HTML Sonderzeichen',
+    MORE                                        : 'mehr...',
+    CLICK_THE_CHARACTER_YOU_WISH_TO_INSERT      : 'Klicke auf das Zeichen, das du einfügen möchtest.',
+    CANCEL                                      : 'Abbrechen'
+});
