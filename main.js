506376f34150721246bc557c9952f46cfb690ea1
Gregory Jackson <gregory@fates.co.uk>
2017-05-12 14:14:35 -0400

    Changed inline functions to arrow functions
    Adopted const/let.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012 Gregory Jackson. All rights reserved.
+ * Copyright (c) 2012-2017 Gregory Jackson. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -21,16 +21,16 @@
  *
  */
 
-/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50, esversion:6 */
 /*global define, $, brackets, event, showFullListDialog */
 
-/* Special HTML Characters Extension 
-    to insert special HTML character codes
-*/
+/* Special HTML Characters Extension
+ to insert special HTML character codes
+ */
 define(function (require, exports, module) {
     'use strict';
 
-    var
+    const
         CommandManager = brackets.getModule('command/CommandManager'),
         Menus = brackets.getModule('command/Menus'),
         EditorManager = brackets.getModule('editor/EditorManager'),
@@ -40,84 +40,66 @@ define(function (require, exports, module) {
         specialChars = JSON.parse(SpecialChars),
         COMMAND_ID = 'specialhtmlchar.insert',
         FULLCOMMAND_ID = 'full.specialhtmlchar.insert',
-        iChars,
-        iCharsLen = specialChars.quickLinks.length,
-        menu, editMenu, keyCommands,
-        $specialCharsDialog = $('<div>', { 'class': 'specialHTMLChar' }),
-        $fullSpecialCharsDialog = $('<div>', { 'class': 'specialHTMLChar full' }),
-        dialogDimensions,
+        $specialCharsDialog = $('<div>', {class: 'specialHTMLChar'}),
+        $fullSpecialCharsDialog = $('<div>', {class: 'specialHTMLChar full'}),
         Strings = require('strings');
-    
+
+    let menu, editMenu, dialogDimensions, fullSpecialCharsDialogCodeDisplay;
+
     ExtensionUtils.loadStyleSheet(module, 'style.css');
-    
+
     /* Construction of popup dialog and events */
     $specialCharsDialog.append(
-        function () {
-            var $ULElement = $('<ul>', { 'class': 'quick' });
-            for (iChars = 0; iChars < iCharsLen; iChars++) {
-                $ULElement.append(
-                    $('<li>').append(
-                        $('<a>', { 'href': '#', 'htmlcode': specialChars.quickLinks[iChars].code, 'text': specialChars.quickLinks[iChars].name }).prepend(
-                            $('<span>', { 'class': 'quickChar', 'html': specialChars.quickLinks[iChars].code })
-                        )
+        $('<ul>', {class: 'quick'}).append(
+            specialChars.quickLinks.map(char => {
+                return $('<li>').append(
+                    $('<a>', {
+                        href: '#',
+                        htmlCode: char.code,
+                        text: char.name
+                    }).prepend(
+                        $('<span>', {class: 'quickChar', 'html': char.code})
                     )
                 );
-            }
-            return $ULElement;
-        }
-    ).append(
-        $('<div>', { 'class': 'divider' }),
-        $('<a>', { 'href': '#', 'class': 'more', 'text': Strings.MORE}).on('click', function () {
+            })
+        ),
+        $('<div>', {class: 'divider'}),
+        $('<a>', {href: '#', class: 'more', text: Strings.MORE}).on('click', function () {
             showFullListDialog();
             $specialCharsDialog.hide();
         })
     ).hide().appendTo('body').on('mouseleave', function () {
         $specialCharsDialog.hide();
-    }).find('ul a').on('click', function () {
-        var
-            cCode = $(this).attr('htmlcode'),
-            doc = DocumentManager.getCurrentDocument(),
-            editor = EditorManager.getCurrentFullEditor(),
-            pos = editor.getCursorPos();
-        doc.replaceRange(cCode, pos);
-        $specialCharsDialog.hide();
-        editor.focus();
-    });
+    }).find('ul a').on('click', writeThisChar);
 
-    iCharsLen = specialChars.full.length;
     $fullSpecialCharsDialog.append(
-        function () {
-            var $ULElement = $('<ul>', { 'class': 'full' });
-            for (iChars = 0; iChars < iCharsLen; iChars++) {
-                $ULElement.append(
-                    $('<li>', { 'title': specialChars.full[iChars].title }).append(
-                        $('<a>', { 'href': '#', 'htmlcode': specialChars.full[iChars].code, 'html': specialChars.full[iChars].code })
-                    )
+        $('<ul>', {class: 'full'}).append(
+            specialChars.full.map(char => {
+                return $('<li>', {'title': char.title}).append(
+                    $('<a>', {
+                        href: '#',
+                        htmlCode: char.code,
+                        html: char.code
+                    })
                 );
-            }
-            return $ULElement;
-        }
-    ).append(
-        $('<div>', { 'class': 'control' }).append(
-            $('<p>', { 'text': Strings.CLICK_THE_CHARACTER_YOU_WISH_TO_INSERT }),
-            $('<input>', { 'type': 'text', 'class': 'codepreview', 'name': 'fullSpecialCharsDialogCodeDisplay', 'value': '' }),
-            $('<a>', { 'href': '#', 'class': 'btn', 'text': Strings.CANCEL }).on('click', function () {
+            })
+        ),
+        $('<div>', {class: 'control'}).append(
+            $('<p>', {text: Strings.CLICK_THE_CHARACTER_YOU_WISH_TO_INSERT}),
+            fullSpecialCharsDialogCodeDisplay = $('<input>', {
+                type: 'text',
+                class: 'codepreview',
+                name: 'fullSpecialCharsDialogCodeDisplay',
+                value: ''
+            }),
+            $('<a>', {href: '#', class: 'btn', text: Strings.CANCEL}).on('click', function () {
                 $fullSpecialCharsDialog.hide();
             })
         )
-    ).hide().appendTo('body').find('ul a').on('click', function () {
-        var
-            cCode = $(this).attr('htmlcode'),
-            doc = DocumentManager.getCurrentDocument(),
-            editor = EditorManager.getCurrentFullEditor(),
-            pos = editor.getCursorPos();
-        doc.replaceRange(cCode, pos);
-        $fullSpecialCharsDialog.hide();
-        editor.focus();
-    }).on('mouseenter', function () {
-        $('[name=fullSpecialCharsDialogCodeDisplay]').val($(this).attr('htmlcode'));
+    ).hide().appendTo('body').find('ul a').on('click', writeThisChar).on('mouseenter', function () {
+        fullSpecialCharsDialogCodeDisplay.val(this.attributes.htmlCode.value);
     }).on('mouseleave', function () {
-        $('[name=fullSpecialCharsDialogCodeDisplay]').val('');
+        fullSpecialCharsDialogCodeDisplay.val('');
     });
 
     dialogDimensions = {
@@ -128,20 +110,34 @@ define(function (require, exports, module) {
 
     function showDialog() {
         $specialCharsDialog.css({
-            'left': ((event.pageX + dialogDimensions.offset) > (event.view.innerWidth - dialogDimensions.width)) ? (event.view.innerWidth - dialogDimensions.width) - 31 : event.pageX - dialogDimensions.offset,
-            'top': ((event.pageY + dialogDimensions.offset) > (event.view.innerHeight - dialogDimensions.height)) ? (event.view.innerHeight - dialogDimensions.height) - 68  : event.pageY - dialogDimensions.offset
+            left: ((event.pageX + dialogDimensions.offset) > (event.view.innerWidth - dialogDimensions.width)) ? (event.view.innerWidth - dialogDimensions.width) - 31 : event.pageX - dialogDimensions.offset,
+            top: ((event.pageY + dialogDimensions.offset) > (event.view.innerHeight - dialogDimensions.height)) ? (event.view.innerHeight - dialogDimensions.height) - 68 : event.pageY - dialogDimensions.offset
         }).show();
     }
 
     function showFullListDialog() {
-        var currentDoc = DocumentManager.getCurrentDocument();
+        const currentDoc = DocumentManager.getCurrentDocument();
+
         if (currentDoc) {
             $fullSpecialCharsDialog.css({
                 'margin-left': -(622 / 2),
                 'margin-top': -(520 / 2),
-                'width': 620
+                width: 620
             }).show();
         }
+    }
+
+    /* this function is called within the scope of the click event target */
+    function writeThisChar() {
+        const
+            doc = DocumentManager.getCurrentDocument(),
+            editor = EditorManager.getCurrentFullEditor(),
+            pos = editor.getCursorPos();
+
+        doc.replaceRange(this.attributes.htmlCode.value, pos);
+
+        $fullSpecialCharsDialog.hide();
+        editor.focus();
     }
 
     CommandManager.register(Strings.SPECIAL_HTML_CHARACTER, COMMAND_ID, showDialog);
